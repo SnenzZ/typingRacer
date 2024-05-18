82c053a3cef8acf817d5b457e998a21c0983bacd
duc1606 <n.rohr@campus.tu-berlin.de>
2024-05-18 16:22:56 +0200

    [IMP] Setting up a basic pygame game

---
+++ typingRacer.py
@@ -0,0 +1,9 @@
+# Typing Racer Game in Python with Pygame!
+import pygame,random, copy
+# pip install pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Typing Racer in Python!')
